9f7ccca9cc29713182ddc952895750690d17c3b9
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-06-28 10:51:53 +0200

    Create Container component

--- fittracker/src/App.js
+++ fittracker/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import {TopBar} from "./components/TopBar/TopBar";
+import {Container} from "./components/Container/Container";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Container />
     </div>
   );
 }
