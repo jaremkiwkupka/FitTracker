1a0bfe41400736707a4f9e735a68b79cca67fd6a
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-06-29 11:03:33 +0200

    Add FontAwesome icons to Menu items

--- fittracker/src/components/Container/Menu/Menu.js
+++ fittracker/src/components/Container/Menu/Menu.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Menu.scss";
-// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalculator, faAppleAlt, faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Menu = () => {
+    const note = <FontAwesomeIcon icon={faCalculator} />
+    const apple = <FontAwesomeIcon icon={faAppleAlt} />
+    const author = <FontAwesomeIcon icon={faUserAstronaut} />
+
     return (
         <>
             <div className="menu-container">
-                {/*<FontAwesomeIcon icon={["fas", "coffee"]} />*/}
-                <p className="menu-item">Calculate your needs</p>
-                <p className="menu-item">Your diary</p>
-                <p className="menu-item">About author</p>
+                <p className="menu-item">{note} Calculate your needs</p>
+                <p className="menu-item">{apple} Your diary</p>
+                <p className="menu-item">{author} About author</p>
             </div>
         </>
     )
