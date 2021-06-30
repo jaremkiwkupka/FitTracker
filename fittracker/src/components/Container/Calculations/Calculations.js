9a37c1e1d5d8e0cdc65311a20899da564a001f6e
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-06-30 12:38:57 +0200

    Calculate the BMI index

--- fittracker/src/components/Container/Calculations/Calculations.js
+++ fittracker/src/components/Container/Calculations/Calculations.js
@@ -1,19 +1,11 @@
 import React from "react";
 import "./Calculations.scss";
 import {Form} from "./Form/Form";
-import {Scores} from "./Scores/Scores";
-import {CaloricContentOfDiet} from "./CaloricContentOfDiet/CaloricContentOfDiet";
-import {SaveCalculations} from "./SaveCalculations/SaveCalculations";
 
 export const Calculations = () => {
     return (
         <section className="main-container">
             <Form />
-            <div className="calculations-container">
-                <Scores />
-                <CaloricContentOfDiet />
-                <SaveCalculations />
-            </div>
         </section>
     )
 }
--- fittracker/src/components/Container/Calculations/Scores/Scores.js
+++ fittracker/src/components/Container/Calculations/Scores/Scores.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../Scores/Scores.scss";
 
-export const Scores = () => {
+export const Scores = ({bmiValue}) => {
     return (
         <div className="scores-wrapper">
-            <p className="scores-item">BMI</p>
+            <p className="scores-item">BMI {bmiValue}</p>
             <p className="scores-item">CPM</p>
             <p className="scores-item">PPM</p>
         </div>
