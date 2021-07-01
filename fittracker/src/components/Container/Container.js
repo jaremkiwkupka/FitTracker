e49366c8752d28e24b35ec0c301615e81426d130
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-07-01 15:30:30 +0200

    Create and render Footer component

--- fittracker/src/components/Container/Container.js
+++ fittracker/src/components/Container/Container.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./Container.scss";
 import {Menu} from "./Menu/Menu";
 import {Calculations} from "./Calculations/Calculations";
+import {Footer} from "../Footer/Footer";
+
 
 export const Container = () => {
+
     return (
-        <div className="container">
-            <Menu />
-            <Calculations />
-        </div>
+        <>
+            <div className="container">
+                <Menu />
+                <Calculations />
+            </div>
+            <Footer />
+        </>
     )
 }
