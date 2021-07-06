2fed7796a969f63de5eb7e83d137624e6f6a1059
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-07-06 15:56:50 +0200

    Create Diary component

--- fittracker/src/components/Diary/Diary.js
+++ fittracker/src/components/Diary/Diary.js
@@ -1,5 +1,22 @@
 import React from "react";
+import {Menu} from "../Container/Menu/Menu";
+import "../../styles/_layout.scss";
+import "../Diary/Diary.scss";
+import {Date} from "./Date/Date";
+import {Meals} from "./Meals/Meals";
+
 
 export const Diary = () => {
-    return <></>
+    return (
+        <div className="container">
+            <Menu />
+            <div className="diary-container">
+                <h2 className="heading">Your Diary</h2>
+                <p className="paragraph">select date and add your meals</p>
+
+                <Date />
+                <Meals />
+            </div>
+        </div>
+    );
 }
