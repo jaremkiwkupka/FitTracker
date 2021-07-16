5028a601fee27571194fb548786c4a45ea0c5c9a
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-07-16 15:32:20 +0200

    Beautify the code

--- fittracker/src/components/Diary/Date/Date.js
+++ fittracker/src/components/Diary/Date/Date.js
@@ -3,12 +3,19 @@ import "../Date/Date.scss";
 
 export const Date = ({date, setDate}) => {
 
+    const handleChange = (e) => {
+        setDate(e.target.value)
+    }
+
     return (
         <div className="diary-date">
             <h3 className="diary-date--heading">{date}</h3>
 
-            <input className="diary-date--input" type="date" id="calendar" name="calendar"
-                   onChange={e => setDate(e.target.value)}
+            <input className="diary-date--input"
+                   type="date"
+                   id="calendar"
+                   name="calendar"
+                   onChange={handleChange}
                    min="2021-01-01" max="2025-12-31"/>
         </div>
     )
