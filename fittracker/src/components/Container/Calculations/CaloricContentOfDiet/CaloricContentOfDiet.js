d0282b0b402e67c0e5807d33612ae5223d69420f
Julia Jaremkiw-Kupka <julka.jaremkiw@gmail.com>
2021-07-16 15:36:03 +0200

    Change the text

--- fittracker/src/components/Container/Calculations/CaloricContentOfDiet/CaloricContentOfDiet.js
+++ fittracker/src/components/Container/Calculations/CaloricContentOfDiet/CaloricContentOfDiet.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import "./CaloricContentOfDiet.scss";
 
 export const CaloricContentOfDiet = ({caloricContent}) => {
 
     return (
         <div className="caloricContent-wrapper">
-            <p className="caloricContent-text">Your recommended caloric intake for the diet:</p>
+            <p className="caloricContent-text">To loose weight you need to eat:</p>
             <span className="caloricContent-value">{` ${caloricContent} kcal`}</span>
         </div>
     )
